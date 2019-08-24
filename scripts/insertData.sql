738ef3da783871f0e06471ad1434626a15468db4
martinperez2306 <perezmartincontacto@gmail.com>
2019-08-24 17:26:15 -0300

    Agregamos scripts de insert

---
+++ scripts/insertData.sql
@@ -0,0 +1,4 @@
+INSERT INTO paradas (cod_parada, longitud, latitud, tipo_parada, calle, altura, entre1, entre2) 
+VALUES (1,1234556789,987654321,'Tipo 1','Calle 1',1400, 'Entre 1', 'Entre 2');
+
+INSERT INTO colectivos_por_parada (cod_parada,num_colectivo) VALUES (1,93);
